4da48c85308f68092e713fa17ab19f29807530af
neuli <asdfz888@naver.com>
2022-03-19 20:30:53 +0900

    Using Content Views #7
    Conform to the Content View Protocol

---
+++ Today/Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by 하늘이 on 2022/03/19.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
