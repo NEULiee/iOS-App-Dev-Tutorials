b57ba3157b3f1777d25d48d965c1067580afdbb9
neuli <asdfz888@naver.com>
2022-04-08 15:54:04 +0900

    Loading Reminders #10
    Convert Between Model Types

--- Today/Today/Models/EKEventStore+AsyncFetch.swift
+++ Today/Today/Models/EKEventStore+AsyncFetch.swift
@@ -14,6 +14,10 @@ extension EKEventStore {
     // async : 이 함수가 비동기적으로 실행될 수 있다.
     func fetchReminders(matching predicate: NSPredicate) async throws -> [EKReminder] {
         
+        /// withCheckedThrowingContinuation : Swift 5.5 동시성
+        /// Checked 가 붙으면 동시 실행 시 단일 실행을 보장하는 함수라는 특징이 있다. (Thread-Safety)
+        /// continuation : CheckedContinuation 구조체
+        /// resume(returing:) - 태스크 반환 값을 조작할 수 있다.
         try await withCheckedThrowingContinuation { continuation in
             fetchReminders(matching: predicate) { reminders in
                 if let reminders = reminders {
